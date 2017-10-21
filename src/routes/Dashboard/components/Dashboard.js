fffee8af773b173a4ac50d6622e60961111390ab
Xinhe <ren7722c@gmail.com>
2017-10-21 22:18:00 +0800

    dashboard page ui

--- src/routes/Dashboard/components/Dashboard.js
+++ src/routes/Dashboard/components/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Loading from "../../../components/Loading";
 import { browserHistory } from "react-router";
-import { Card } from "antd";
+import { Card, Progress, Icon } from "antd";
 import "./Dashboard.scss";
 
 import {
@@ -24,7 +24,8 @@ export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true
+      isLoading: true,
+      latest_percentage: 0
     };
   }
   componentWillMount() {}
@@ -34,7 +35,14 @@ export default class Dashboard extends React.Component {
         isLoading: false
       });
     }, 800);
+    this.animatePercentage();
   }
+  animatePercentage = () => {
+    var percent = 89;
+    setTimeout(() => {
+      this.setState({ latest_percentage: percent });
+    }, 1000);
+  };
   render() {
     const isMobile =
       navigator.userAgent.match(/Android/i) ||
@@ -54,48 +62,146 @@ export default class Dashboard extends React.Component {
       { name: "六", val: 53 },
       { name: "日", val: 74 }
     ];
+    const best_memory_data = [
+      { name: "", 黃小明: 50, 陳小花: 200 },
+      { name: "9/2", 黃小明: 90, 陳小花: 150 },
+      { name: "9/5", 黃小明: 65, 陳小花: 130 },
+      { name: "9/7", 黃小明: 80, 陳小花: 70 },
+      { name: "9/8", 黃小明: 95, 陳小花: 68 },
+      { name: "9/11", 黃小明: 88, 陳小花: 100 },
+      { name: "9/15", 黃小明: 120, 陳小花: 160 }
+    ];
     return (
       <div>
         {isLoading && <Loading />}
         <div>
-          <Card
-            title="個人平均狀態記憶力"
-            style={{ width: "100%" }}
-            className="dashboard-index-card"
-          >
-            <ResponsiveContainer aspect={2.5}>
-              <BarChart data={personal_average_memory_data}>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" vertical={false} />
-                <Tooltip />
-                <Legend iconType="circle" />
-                {!isLoading ? (
-                  <Bar dataKey="val" fill="#9ACBD9" animationDuration={2000} />
-                ) : null}
-              </BarChart>
-            </ResponsiveContainer>
-          </Card>
-          <Card
-            title="一周內記憶力最佳"
-            style={{ width: "45%" }}
-            className="dashboard-index-card"
-          >
-            <p>星期日</p>
-            <p>平均記憶力指數：74分</p>
-          </Card>
-          <Card
-            title="一天內記憶力最佳"
-            style={{ width: "45%" }}
-            className="dashboard-index-card"
-          >
-            <p>14：00</p>
-            <p>平均記憶力指數：85分</p>
-          </Card>
+          <div className="row">
+            <div className="col-md-9">
+              <Card
+                title="個人平均狀態記憶力"
+                style={{ width: "100%" }}
+                className="dashboard-index-card avg-data-wrapper"
+              >
+                <ResponsiveContainer aspect={2.3}>
+                  <BarChart data={personal_average_memory_data}>
+                    <XAxis dataKey="name" tickLine={false} />
+                    <YAxis tickLine={false} />
+                    <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                    <Tooltip />
+                    <Legend iconType="circle" />
+                    {!isLoading ? (
+                      <Bar
+                        dataKey="val"
+                        fill="#9ACBD9"
+                        animationDuration={2000}
+                      />
+                    ) : null}
+                  </BarChart>
+                </ResponsiveContainer>
+              </Card>
+            </div>
+            <div className="col-md-3">
+              <div className="row">
+                <div className="col-md-12">
+                  <Card
+                    title="最新數據"
+                    style={{ width: "100%" }}
+                    className="dashboard-index-card latest-data-wrapper"
+                  >
+                    <p className="latest-data-name">黃小明</p>
+                    <p className="latest-data-time">2017/09/12 18:22 睡覺前</p>
+                    <Progress
+                      type="circle"
+                      percent={this.state.latest_percentage}
+                      format={percent => `${percent}`}
+                      strokeWidth={5}
+                    />
+                    <p className="progress-gain">
+                      <Icon type="caret-up" /> 34
+                    </p>
+                  </Card>
+                </div>
+                <div className="col-md-12">
+                  <Card
+                    title="本週最佳表現"
+                    style={{ width: "100%" }}
+                    className="dashboard-index-card best-data-wrapper"
+                  >
+                    <span>
+                      <Icon type="calendar" /> 週五
+                    </span>　　<span>
+                      <Icon type="user" /> 陳小花
+                    </span>
+                  </Card>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <Card
+                title="各狀態平均記憶力"
+                style={{ width: "100%" }}
+                className="dashboard-index-card"
+              >
+                <ResponsiveContainer aspect={1.5}>
+                  <BarChart
+                    data={personal_average_memory_data}
+                    layout="vertical"
+                  >
+                    <XAxis dataKey="val" type="number" tickLine={false} />
+                    <YAxis dataKey="name" type="category" tickLine={false} />
+                    <CartesianGrid strokeDasharray="3 3" horizontal={false} />
+                    <Tooltip />
+                    {!isLoading ? (
+                      <Bar
+                        dataKey="val"
+                        fill="#9ACBD9"
+                        animationDuration={2000}
+                      />
+                    ) : null}
+                  </BarChart>
+                </ResponsiveContainer>
+              </Card>
+            </div>
+            <div className="col-md-6">
+              <Card
+                title="一天內記憶力最佳"
+                style={{ width: "100%" }}
+                className="dashboard-index-card"
+              >
+                <ResponsiveContainer aspect={1.5}>
+                  <LineChart data={best_memory_data} className="linechart">
+                    <XAxis dataKey="name" tickLine={false} />
+                    <YAxis tickLine={false} />
+                    <CartesianGrid strokeDasharray="3 3" stroke="#B4DAE5" />
+                    <Tooltip />
+                    <Legend iconType="circle" />
+                    {!isLoading ? (
+                      <Line
+                        type="monotone"
+                        dataKey="黃小明"
+                        stroke="#F2992E"
+                        strokeWidth="4"
+                        dot={{ stroke: "#F2992E", strokeWidth: 4 }}
+                        animationDuration={2000}
+                      />
+                    ) : null}
+                    {!isLoading ? (
+                      <Line
+                        type="monotone"
+                        dataKey="陳小花"
+                        stroke="#F5808B"
+                        strokeWidth="4"
+                        dot={{ stroke: "#F5808B", strokeWidth: 4 }}
+                        animationDuration={2000}
+                      />
+                    ) : null}
+                  </LineChart>
+                </ResponsiveContainer>
+              </Card>
+            </div>
+          </div>
         </div>
-        <hr />
-        <br />
-        個人記憶力
       </div>
     );
   }
